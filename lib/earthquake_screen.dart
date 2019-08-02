3d239024635fcad48b2ac6aa98c9e49d0fec7de1
Souvik Biswas <sbis1999@vorpy.me>
2019-08-02 14:14:15 +0530

    Add License

--- lib/earthquake_screen.dart
+++ lib/earthquake_screen.dart
@@ -1,3 +1,23 @@
+// Copyright (c) 2019 Souvik Biswas
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -14,7 +34,7 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
   final String url =
       'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=20';
 
-  List earthquakes;
+  // List earthquakes;
 
   @override
   void initState() {
@@ -22,7 +42,9 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
     this.getJson();
   }
 
-  Future<dynamic> getJson() async {
+  Future<List> getJson() async {
+    List earthquakes;
+
     var response = await http.get(
       Uri.encodeFull(url),
       headers: {'Accept': 'application.json'},
@@ -37,25 +59,30 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
       });
     }
 
-    return 'Success';
+    return earthquakes;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Quake Report'),
-      ),
-      body: earthquakes == null
-          ? Container(
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            )
-          : ListView.builder(
-              itemCount: earthquakes == null ? 0 : earthquakes.length,
+        appBar: AppBar(
+          title: Text('Quake Report'),
+        ),
+        body: FutureBuilder(
+          future: getJson(),
+          builder: (context, snapshot) {
+            if (snapshot.data == null) {
+              return Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.hasData ? snapshot.data.length : 0,
               itemBuilder: (_, index) {
-                Map properties = earthquakes[index]['properties'];
+                Map properties = snapshot.data[index]['properties'];
                 var magnitude = properties['mag'].toDouble();
                 var place = properties['place'];
                 var time = properties['time'];
@@ -80,7 +107,44 @@ class _EarthquakeScreenState extends State<EarthquakeScreen> {
                   ),
                 );
               },
-            ),
-    );
+            );
+          },
+        )
+        // earthquakes == null
+        //     ? Container(
+        //         child: Center(
+        //           child: CircularProgressIndicator(),
+        //         ),
+        //       )
+        //     : ListView.builder(
+        //         itemCount: earthquakes == null ? 0 : earthquakes.length,
+        //         itemBuilder: (_, index) {
+        //           Map properties = earthquakes[index]['properties'];
+        //           var magnitude = properties['mag'].toDouble();
+        //           var place = properties['place'];
+        //           var time = properties['time'];
+        //           var quakeUrl = properties['url'];
+
+        //           return Container(
+        //             padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+        //             child: Card(
+        //               color: Colors.teal[100],
+        //               child: Padding(
+        //                 padding: const EdgeInsets.all(20.0),
+        //                 child: Column(
+        //                   children: <Widget>[
+        //                     Text(place),
+        //                     Text(magnitude.toString()),
+        //                     Text(DateFormat.yMMMd().format(
+        //                         DateTime.fromMillisecondsSinceEpoch(time))),
+        //                     Text(quakeUrl),
+        //                   ],
+        //                 ),
+        //               ),
+        //             ),
+        //           );
+        //         },
+        //       ),
+        );
   }
 }
